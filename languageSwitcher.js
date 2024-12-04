722b619d3e6ca217a313895ff340337a5f310a0d
Jor <michal.jor96@gmail.com>
2024-12-04 20:46:26 +0100

    adding cs variant to html and md

--- languageSwitcher.js
+++ languageSwitcher.js
@@ -1,9 +1,19 @@
-const languageSwitchButton = document.getElementById('language-switch');
-
-languageSwitchButton.addEventListener('click', () => {
-  if (languageSwitchButton.textContent === 'Switch language') {
-    languageSwitchButton.textContent = 'Změnit jazyk';
-  } else {
-    languageSwitchButton.textContent = 'Switch language';
-  }
+document.getElementById('language-switch').addEventListener('click', function() { 
+  changeLanguage('en');
+  changeLanguage('cs')
 });
+function changeLanguage(language) {
+  if (language === 'cs') {
+      fetch('index-cs.html')
+          .then(response => response.text())
+          .then(data => {
+              document.body.innerHTML = data;
+          });
+  } else if (language === 'en') {
+      fetch('index-en.html')
+          .then(response => response.text())
+          .then(data => {
+              document.body.innerHTML = data;
+          });
+  }
+}
